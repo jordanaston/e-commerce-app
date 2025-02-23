634bf1feebbd667dd94fc7359f726047e8287463
Jordan Aston <jordanaston93@gmail.com>
2025-02-23 11:52:48 +1000

    business logo svg added to navbar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,13 +1,23 @@
+import Image from "next/image";
 import DeliveryLocation from "./DeliveryLocation";
 import MenuButtons from "./MenuButtons";
 import SearchBar from "./SearchBar";
 
 export default function NavBar() {
   return (
-    <div className="flex justify-end p-8 bg-accent-foreground">
-      <DeliveryLocation />
-      <SearchBar />
-      <MenuButtons />
-    </div>
+    <>
+      <div className="flex justify-between p-8 bg-accent-foreground">
+        <Image
+          src="/business-name.svg"
+          alt="Business Name"
+          className="mr-8"
+          width={120}
+          height={0}
+        />
+        <DeliveryLocation />
+        <SearchBar />
+        <MenuButtons />
+      </div>
+    </>
   );
 }
