0ddb7f76e2b8e7803d2f1937f1c5b35d09f72088
Jordan Aston <jordanaston93@gmail.com>
2025-02-23 15:19:30 +1000

    item card built to be responsive

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Product } from "@/types/product";
+import { FaStar } from "react-icons/fa";
+import { Button } from "./ui/button";
 
 export default function ItemCard({ product }: { product: Product }) {
   return (
-    <div className="flex flex-wrap gap-4 justify-center">
+    <div className="flex flex-col flex-wrap justify-center">
       <div
         key={product.id}
-        className="flex flex-col bg-white w-[250px] h-[350px] p-4 shadow-md items-center justify-center "
+        className="flex flex-col bg-white w-[250px] h-[350px] shadow-md items-center justify-center"
       >
         <Image
           src={product.image}
@@ -15,6 +17,23 @@ export default function ItemCard({ product }: { product: Product }) {
           height={100}
           className="mx-auto"
         />
+      </div>
+      <div className="flex flex-col justify-between bg-grey-200 w-[250px] h-72 p-4 shadow-lg">
+        <p className="text-2xl font-bold">${product.price}</p>
+        <div className="flex flex-col justify-start min-h-36">
+          <p className="text-sm">{product.title}</p>
+          <p className="text-sm font-medium capitalize">{product.category}</p>
+          <div className="flex mt-2">
+            {Array.from({ length: Math.round(product.rating.rate) }).map(
+              (_, index) => (
+                <FaStar key={index} color="grey" />
+              )
+            )}
+          </div>
+        </div>
+        <Button className="flex justify-center items-center p-4">
+          Add to Cart
+        </Button>
       </div>
     </div>
   );
