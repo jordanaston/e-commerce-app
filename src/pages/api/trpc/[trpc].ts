ad2960682b1de56f3845fd32f2de63584c470aa5
Jordan Aston <jordanaston93@gmail.com>
2025-03-13 22:04:52 +1000

    fix 502 errors in deployment

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,16 @@
 import { appRouter } from "@/server/routers/_app";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { createContext } from "@/server/trpc";
+import dbConnect from "@/lib/mongoose";
+
+dbConnect().catch(console.error);
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
+  onError({ error }) {
+    if (error.code === "INTERNAL_SERVER_ERROR") {
+      console.error("Something went wrong", error);
+    }
+  },
 });
