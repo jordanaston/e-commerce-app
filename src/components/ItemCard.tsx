af20992cceba7a178f473a2a8cbdd348c02f9528
Jordan Aston <jordanaston93@gmail.com>
2025-03-16 13:50:43 +1000

    fixed responsiveness for items cards on home screen to show 2 columns on smaller devices

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -36,10 +36,10 @@ const ItemCard = ({ product }: { product: Product }) => {
     <div className="flex flex-col flex-wrap justify-center">
       <div
         key={product.id}
-        className="flex flex-col bg-white w-[250px] h-[350px] shadow-md items-center justify-center relative animated-border"
+        className="flex flex-col bg-white w-[180px] h-[250px] sm:w-[250px] sm:h-[350px] shadow-md items-center justify-center relative animated-border"
       >
         <Link href={`/product?id=${product.id}`} className="">
-          <div className="relative w-[150px] h-[150px]">
+          <div className="relative w-[100px] h-[100px] sm:w-[150px] sm:h-[150px]">
             <Image
               src={product.image}
               alt={product.title}
@@ -51,15 +51,21 @@ const ItemCard = ({ product }: { product: Product }) => {
           </div>
         </Link>
       </div>
-      <div className="flex flex-col justify-between bg-grey-200 w-[250px] h-72 p-4 shadow-lg">
-        <p className="text-2xl font-bold">${product.price}</p>
+      <div className="flex flex-col justify-between bg-grey-200 w-[180px] h-[250px] sm:w-[250px] sm:h-[350px] p-4 shadow-lg">
+        <p className="text-md sm:text-2xl font-bold">${product.price}</p>
         <div className="flex flex-col justify-start min-h-36">
-          <p className="text-sm">{product.title}</p>
-          <p className="text-sm font-medium capitalize">{product.category}</p>
+          <p className="text-xs sm:text-sm">{product.title}</p>
+          <p className=" text-xs sm:text-sm font-medium capitalize">
+            {product.category}
+          </p>
           <div className="flex mt-2">
             {Array.from({ length: Math.round(product.rating.rate) }).map(
               (_, index) => (
-                <FaStar key={index} color="grey" />
+                <FaStar
+                  key={index}
+                  color="grey"
+                  className="w-3 h-3 sm:w-4 sm:h-4"
+                />
               )
             )}
           </div>
